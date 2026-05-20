7d49f53e909be28955933d29eccb118676cfcb80
suvas16 <115240689+suvas16@users.noreply.github.com>
2026-05-20 23:08:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-
+const io = new Server(server, {
+    maxHttpBufferSize: 1e7,  
+    transports: ['websocket'] 
+});
 const app = express();
 const server = http.createServer(app);
 
